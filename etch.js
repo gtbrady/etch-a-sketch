67e72df35c6513aeea7d1b5c3d8c2c9dd584003a
gtbrady <97182053+gtbrady@users.noreply.github.com>
2026-05-13 09:11:26 -0500

    Add flexbox to container to center elements

--- etch.js
+++ etch.js
@@ -2,9 +2,13 @@ const container = document.querySelector("#container");
 
 function createGrid(integer) {
     for(let i = 0; i < integer; i++) {
+        for(let j = 0; j < integer; j++) {
         const div = document.createElement("div");
-        div.innerText = "Div#:" + i
+        div.innerText = i + "|" + j;
+        div.classList.add("grid-item");
         container.appendChild(div);
+        }
+
     }
     
 }
