f82e33b661df0924b858fb5cc0e85e2ad248279c
gtbrady <97182053+gtbrady@users.noreply.github.com>
2026-05-13 09:43:04 -0500

    Add dynamic sizing to grid items

--- etch.js
+++ etch.js
@@ -1,16 +1,19 @@
 const container = document.querySelector("#container");
+const gridSize = container.clientWidth;
 
 function createGrid(integer) {
+    container.replaceChildren();
     for(let i = 0; i < integer; i++) {
         for(let j = 0; j < integer; j++) {
         const div = document.createElement("div");
-        div.innerText = i + "|" + j;
         div.classList.add("grid-item");
+        div.style.height = `${gridSize/integer}px`;
+        div.style.width = `${gridSize/integer}px`;
         container.appendChild(div);
         }
-
-    }
-    
+    }   
 }
 
-createGrid(4);
+
+
+createGrid(16);
