289225753534444a4e64975b1045cbea9659f0d3
gtbrady <97182053+gtbrady@users.noreply.github.com>
2026-05-14 09:36:45 -0500

    Add buttons to change grid size and reset grid
    Added a Grid Detail button which allows the user to change the level of
    detail of each div.
    
    Also added a reset button that reverts the grid color back to the
    original style.

--- etch.js
+++ etch.js
@@ -1,5 +1,24 @@
 const container = document.querySelector("#container");
 const gridSize = container.clientWidth;
+const gridDetailButton = document.querySelector("#grid-detail-button");
+const resetButton = document.querySelector("#reset-button");
+
+gridDetailButton.addEventListener("click", () => {
+    const pixelDetail = Number(prompt("Enter a number 1 to 100 to change the pixel count of the grid:"));
+    if(Number.isInteger(pixelDetail) &&
+        (pixelDetail >= 0 && pixelDetail <= 100)) {
+            createGrid(pixelDetail);
+        }
+    else {
+        alert("Please enter a number from 1 to 100.");
+    }
+});
+
+resetButton.addEventListener("click", () => {
+    for(const div of container.children) {
+        div.style.backgroundColor = "";
+    }
+});
 
 function createGrid(integer) {
     container.replaceChildren();
@@ -9,11 +28,6 @@ function createGrid(integer) {
         div.classList.add("grid-item");
         div.style.height = `${gridSize/integer}px`;
         div.style.width = `${gridSize/integer}px`;
-        /*
-        I think for the extra credit I can pass a different
-        event listener function depending on the "mode". Random
-        color or Opacity
-        */
         div.addEventListener("mouseenter", ()=> {
             div.style.backgroundColor = "black";
         });
